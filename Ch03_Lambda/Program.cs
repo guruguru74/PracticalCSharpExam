0a0e7a48c30b11737ea2c207c76c703e8324be2c
SangKyu, Choi <sangkyu.choi@kohyoung.com>
2025-01-31 08:55:17 +0900

    Initial Commit.

---
+++ Ch03_Lambda/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch03_Lambda
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            cs31 ins = new cs31();
+            Console.WriteLine($"{ins.Count(5)}");
+
+
+        }
+
+
+    }
+}
---
+++ Ch03_Lambda/cs31.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch03_Lambda
+{
+    internal class cs31
+    {
+        public int Count(int num)
+        {
+            var numbers = new[] { 5, 3, 9, 6, 7, 5, 8, 1, 0, 5, 10, 4 };
+            int count = 0;
+            foreach (var n in numbers)
+            {
+                if (n == num)
+                    count++;
+            }
+
+            return count;
+        }
+
+        public delegate bool Judgment(int value);
+        public int Count(int[] numbers, Judgment judge)
+        {
+            int count = 0;
+            foreach (var n in numbers)
+            {
+                if (judge(n) == true)
+                    count++;
+            }
+            return count;
+        }
+    }
+}
