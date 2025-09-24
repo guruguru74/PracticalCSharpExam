e2f6be55f8abab8f45df6fccce31c41627b4a28e
guruguru74 <guruguru74@gmail.com>
2025-09-24 23:00:27 +0900

    Revert "13.12 데이터 주석과 자동 마이크레이션"
    This reverts commit edfd51382c6135d0668f37f7115bec5a9cdfb0c3.

--- Ch13_SampleEntityFramework/Models/Books.cs
+++ Ch13_SampleEntityFramework/Models/Books.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,12 +9,8 @@ namespace Ch13_SampleEntityFramework.Models
     public class Book
     {
         public int Id { get; set; }
-        [Required]
         public string Title { get; set; }
-        [MaxLength(16)]
-        public string Publisher { get; set; }
-
-        public int? PublishedYear { get; set; }
+        public int PublishedYear { get; set; }
 
         public virtual Author Author { get; set; }
         
